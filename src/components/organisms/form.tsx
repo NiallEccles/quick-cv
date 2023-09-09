103dc683bb246881ea71835ccb0b1738b7e5308d
EcclesN <Niall.Eccles@hmhco.com>
2023-09-09 23:26:58 +0100

    Add padding

--- src/components/organisms/form.tsx
+++ src/components/organisms/form.tsx
@@ -34,7 +34,7 @@ export default function FormContainer() {
   }
 
   return (<Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 px-5">
             <FormField
                 control={form.control}
                 name="username"
