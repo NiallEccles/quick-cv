3432a6dd9eab5916d15134fe7526ed0bed5cf5db
EcclesN <Niall.Eccles@hmhco.com>
2023-09-11 22:38:22 +0100

    Removes import

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,8 +1,7 @@
 import PersonalDetails from '@/components/organisms/form'
-import Image from 'next/image'
 
 export default function Create() {
   return (
-    <PersonalDetails/>
+    <PersonalDetails />
   )
 }
