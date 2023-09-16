979312d681bdeb2d6095378c0d1b9efe07c9c536
EcclesN <Niall.Eccles@hmhco.com>
2023-09-16 16:04:34 +0100

    Switch to React

--- src/app/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Dynamic } from "@/components/organisms/dynamic";
-import Personal from "@/components/organisms/form";
-
-export default function Create() {
-  return (
-    // <Personal />
-    <Dynamic />
-  )
-}
