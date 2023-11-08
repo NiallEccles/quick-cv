f4466023d863a608525072ad5bc65b2547143f6e
EcclesN <Niall.Eccles@hmhco.com>
2023-11-08 20:58:12 +0000

    Migrate to nx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,7 +6,9 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <div>
-      <NxWelcome title="quick-cv" />
+        <h1 className="bg-indigo-500 p-2 font-mono">hello there</h1>
+        <h2 className="bg-black text-white">general kenobi</h2>
+      {/*<NxWelcome title="quick-cv" />*/}
     </div>
   );
 }
