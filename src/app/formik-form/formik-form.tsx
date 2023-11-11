dbf779a9c2f1d5226cc478a04977e3ad306cb512
EcclesN <Niall.Eccles@hmhco.com>
2023-11-11 18:10:06 +0000

    Working example using formik

--- src/app/formik-form/formik-form.tsx
+++ src/app/formik-form/formik-form.tsx
@@ -1,59 +1,72 @@
 import styles from './formik-form.module.css';
-
-import {Formik, FormikErrors, FormikValues} from "formik";
+import React from 'react';
+import {Form, Formik, FieldArray, Field} from "formik";
 import { Button, Label, TextInput } from 'flowbite-react';
 
 /* eslint-disable-next-line */
 export interface FormikFormProps {}
 
 type ExperienceFields = {
-    school: string;
-    degree: string;
-    dates: string;
-    description: string;
+    experiences: Array<
+        {
+            school: string;
+            degree: string;
+            dates: string;
+            description: string;
+        }
+    >
 }
 
 const initialValues: ExperienceFields = {
-    school: "",
-    degree: "",
-    dates: "",
-    description: ""
+    experiences: [
+        {
+            school: "",
+            degree: "",
+            dates: "",
+            description: ""
+        }
+    ]
 };
 
 export function FormikForm(props: FormikFormProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to FormikForm!</h1>
-        <Formik initialValues={initialValues} onSubmit={(values) => console.log(values)} validate={(values: FormikValues) => {
-            const errors: FormikErrors<ExperienceFields> = {};
-            if (!values.school) {
-                errors.school = "Required";
-            }
-            return errors;
-        }}>
-            {
-                ({ handleSubmit, handleChange, errors, touched}) => (
-                    <form onSubmit={handleSubmit}>
-                        <Label htmlFor="school" value="School/College/University" />
-                        <TextInput name="school" onChange={handleChange}/>
-                        {
-                            touched.school && errors.school
-                                ? <div>{errors.school}</div>
-                                : null
-                        }
-                        <Label htmlFor="degree" value="Degree" />
-                        <TextInput name="degree" onChange={handleChange}/>
+        <Formik
+            initialValues={initialValues}
+            onSubmit={(values) => {
+                console.log(values);
+            }}
+        >
+            {({ values , handleChange}) => (
+                <Form>
+                    <FieldArray name="experiences">
+                        {({ push, remove }) => (
+                            <div>
+                                {values.experiences.map((experience, index) => (
+                                    <div key={index}>
+                                        <Label htmlFor={`experiences.${index}.school`} value="School"/>
+                                        <TextInput name={`experiences.${index}.school`} id={`experiences.${index}.school`} placeholder="School" onChange={handleChange}/>
 
-                        <Label htmlFor="dates" value="Dates" />
-                        <TextInput name="dates" onChange={handleChange}/>
+                                        <Label htmlFor={`experiences.${index}.degree`} value="Degree"/>
+                                        <TextInput name={`experiences.${index}.degree`} id={`experiences.${index}.degree`} placeholder="Degree" onChange={handleChange}/>
 
-                        <Label htmlFor="description" value="Description" />
-                        <TextInput name="description" onChange={handleChange}/>
-                        <br/>
-                        <Button type="submit">Submit</Button>
-                    </form>
-                )
-            }
+                                        <Label htmlFor={`experiences.${index}.dates`} value="Dates"/>
+                                        <TextInput name={`experiences.${index}.dates`} id={`experiences.${index}.dates`} placeholder="Dates" onChange={handleChange}/>
+                                        <Button type="button" color="failure" onClick={() => remove(index)}>
+                                            Remove
+                                        </Button>
+                                    </div>
+                                ))}
+                                <Button type="button" onClick={() => push({ school: '', degree: '', dates: '', description: '' })}>
+                                    Add Experience
+                                </Button>
+                                <br/>
+                            </div>
+                        )}
+                    </FieldArray>
+                    <Button type="submit">Submit</Button>
+                </Form>
+            )}
         </Formik>
     </div>
   );
