f7b26eb5c409e5d1ebc21172a7178debc376758e
EcclesN <Niall.Eccles@hmhco.com>
2023-11-12 22:54:05 +0000

    Add basic personal form

--- src/app/personal-form/personal-form.tsx
+++ src/app/personal-form/personal-form.tsx
@@ -1,12 +1,39 @@
 import styles from './personal-form.module.css';
+import React from "react";
+import {Form, Formik, FieldArray, Field} from "formik";
+import { Button, Label, TextInput, Checkbox } from 'flowbite-react';
+import {ExperienceFields, PersonalFields} from "../types";
+import useInitialValues from "../hooks/useInitialValues";
 
 /* eslint-disable-next-line */
 export interface PersonalFormProps {}
 
 export function PersonalForm(props: PersonalFormProps) {
+    const initialValues: PersonalFields = useInitialValues();
+
   return (
     <div className={styles['container']}>
-      <h1>Welcome to PersonalForm!</h1>
+        <h2>Personal Details</h2>
+        <Formik
+            initialValues={initialValues}
+            onSubmit={(values) => {
+                console.log(values);
+            }}
+        >
+            {({ values: {personalDetails} , handleChange}) => (
+                <Form>
+                    <Label htmlFor={`personalDetails.firstName`} value="First Name"/>
+                    <TextInput
+                        name={`personalDetails.firstName`}
+                        id={`personalDetails.firstName`}
+                        value={personalDetails.firstName}
+                        placeholder="First Name"
+                        onChange={handleChange}
+                    />
+                    <Button type="submit">Submit</Button>
+                </Form>
+            )}
+        </Formik>
     </div>
   );
 }
