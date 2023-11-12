0afa80fd17d9f8ad71cd6aa6807a1ecb6c392255
EcclesN <Niall.Eccles@hmhco.com>
2023-11-12 22:53:36 +0000

    Create types for other fields

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,3 +1,28 @@
+export type PersonalFields = {
+    personalDetails: {
+        firstName: string;
+        lastName: string;
+        jobTitle: string;
+        email: string;
+        phone: string;
+        country: string;
+        city: string;
+    }
+}
+
+export type EmploymentFields = {
+    employments: Array<
+        {
+            title: string;
+            employer: string;
+            currentlyWorking: boolean;
+            startDate: string;
+            endDate: string;
+            description: string;
+        }
+    >
+};
+
 export type ExperienceFields = {
     experiences: Array<
         {
@@ -8,6 +33,51 @@ export type ExperienceFields = {
             startDate: string;
             endDate: string;
             description: string;
+        }
+    >
+}
+
+export type Skills = Array<string>;
+
+export type ReferencesFields = {
+    references: Array<
+        {
+            referee: string;
+            company: string;
+            phone: string;
+            email: string;
+            availableOnRequest: boolean;
+        }
+    >
+};
+
+export type LanguageFields = {
+    languages: Array<
+        {
+            language: string;
+            level:
+                'Native speaker' |
+                'Highly proficient' |
+                'Very good command' |
+                'Good working knowledge' |
+                'C2' |
+                'C1' |
+                'B2' |
+                'B1' |
+                'A2' |
+                'A1'
+        }
+    >
+}
+
+export type CourseFields = {
+    courses: Array<
+        {
+            course: string;
+            institution: string;
+            currentlyStudying: boolean;
+            startDate: string;
+            endDate: string;
         }
     >
 }
