9084cac8137d1e4552397300aa3a5869e8265bf5
EcclesN <Niall.Eccles@hmhco.com>
2023-11-12 20:45:48 +0000

    Create useInitialValues

---
+++ src/app/hooks/useInitialValues.ts
@@ -0,0 +1,18 @@
+const useInitialValues = () => {
+    return {
+        someobj: [],
+        experiences: [
+            {
+                id: '1',
+                school: "1",
+                degree: "1",
+                currentlyStudying: false,
+                startDate: "1",
+                endDate: "1",
+                description: "",
+            },
+        ]
+    }
+};
+
+export default useInitialValues;
