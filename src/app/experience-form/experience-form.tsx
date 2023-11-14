82f3e691bc27d90c09cb85a06f49ad3900abe9c6
EcclesN <Niall.Eccles@hmhco.com>
2023-11-14 23:01:48 +0000

    Rewrite experience form to be leaner

--- src/app/experience-form/experience-form.tsx
+++ src/app/experience-form/experience-form.tsx
@@ -1,114 +1,49 @@
 import styles from './experience-form.module.css';
-import { useForm, useFieldArray, Controller, useWatch, SubmitHandler } from "react-hook-form";
+import { Field, FieldArray } from 'formik';
 import { Button, Checkbox, Label, TextInput, Datepicker } from 'flowbite-react';
+import {ExperienceFields, PersonalFields} from "../types";
+import React from "react";
 
 /* eslint-disable-next-line */
-export interface ExperienceFormProps {}
-
-type Inputs = {
-    school: string
-    degree: string
-    dates?: string
-    description?: string
+export interface ExperienceFormProps {
+    values: ExperienceFields,
+    handleChange: {(e: React.ChangeEvent<any>): void, <T=string | React.ChangeEvent<any>>(field: T): T extends React.ChangeEvent<any> ? void : ((e: (string | React.ChangeEvent<any>)) => void)}
 }
 
+
 export function ExperienceForm(props: ExperienceFormProps) {
-    const onSubmit = (data: any) => console.log(data);
-    const { register, control, handleSubmit, reset, watch } = useForm({
-        defaultValues: {
-            experience: [{ school: "Bill", degree: "Luo", dates: "", description: "" }]
-        }
-    });
-    const {
-        fields,
-        append,
-        remove,
-        move,
-    } = useFieldArray({
-        control,
-        name: "experience"
-    })
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <ol>
-                {fields.map((item, index) => {
-                    return (
-                        <li key={item.id}>
-                            <div className="flex">
-                                <div>
-                                    {/*<TextInput*/}
-                                    {/*    id={`test.${index}.experienceName${index}`}*/}
-                                    {/*    {...register(`test.${index}.experienceName`, { required: true})}*/}
-                                    {/*/>*/}
-                                    <div className="mb-2">
-                                        <Label htmlFor={`test.${index}.experienceName${index}`} value="School/College/University" />
-                                    </div>
-                                    <Controller
-                                        render={({ field }) => <TextInput {...field} />}
-                                        name={`experience.${index}.school`}
-                                        control={control}
-                                    />
-                                    <div className="mb-2">
-                                        <Label htmlFor={`test.${index}.degree${index}`} value="Degree" />
-                                    </div>
-                                    <Controller
-                                        render={({ field }) => <TextInput {...field} />}
-                                        name={`experience.${index}.degree`}
-                                        control={control}
-                                    />
-                                    <div className="mb-2">
-                                        <Label htmlFor={`experience.${index}.dates${index}`} value="Dates" />
-                                    </div>
-                                    <Controller
-                                        render={({ field }) => <Datepicker />}
-                                        name={`experience.${index}.dates`}
-                                        control={control}
-                                    />
-                                    <div className="mb-2">
-                                        <Label htmlFor={`experience.${index}.description${index}`} value="Description" />
-                                    </div>
-                                    <Controller
-                                        render={({ field }) => <TextInput {...field} />}
-                                        name={`experience.${index}.description`}
-                                        control={control}
-                                    />
-                                </div>
+        <FieldArray name="experiences">
+            {({remove, push}) => (
+                <div>
+                    {props.values.length > 0 &&
+                        props.values.map((experience, index) => (
+                            <div key={index}>
+                                <h3>{`${props.values[index].school} - ${props.values[index].degree}`}</h3>
+                                <Label htmlFor={`experiences.${index}.school`} value="School"/>
+                                <TextInput
+                                    name={`experiences.${index}.school`}
+                                    id={`experiences.${index}.school`}
+                                    value={props.values[index].school}
+                                    placeholder="School"
+                                    onChange={props.handleChange}
+                                />
                                 <Button type="button" onClick={() => remove(index)}>
-                                    Delete
+                                    Remove
                                 </Button>
                             </div>
-                        </li>
-                    );
-                })}
-            </ol>
-            <section>
-                <button
-                    type="button"
-                    onClick={() => {
-                        append({ school: "appendBill", degree: "appendLuo", dates: "", description: "" });
-                    }}
-                >
-                    append
-                </button>
-
-                <button type="button" onClick={() => move(0, 1)}>
-                    move
-                </button>
-
-                <button
-                    type="button"
-                    onClick={() =>
-                        reset({
-                            experience: [{ school: "Bill", degree: "Luo" }]
-                        })
+                            )
+                        )
                     }
-                >
-                    reset
-                </button>
-            </section>
-
-            <Button type="submit">Submit</Button>
-        </form>
+                    <Button
+                        type="button"
+                        onClick={() => push({ school: ''  })}
+                    >
+                        Add Experience
+                    </Button>
+                </div>
+                )}
+        </FieldArray>
     );
 }
 
