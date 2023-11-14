89d7bb6402e847b1885a7dc7b63bbf98a032b630
EcclesN <Niall.Eccles@hmhco.com>
2023-11-14 23:02:29 +0000

    Move formik and form to root wrapping all form sections

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,15 +4,30 @@ import styles from './app.module.css';
 // import ExperienceForm from "./experience-form/experience-form";
 import ExperiencesForm from "./experiences-form/experiences-form";
 import PersonalForm from "./personal-form/personal-form";
+import useInitialValues from "./hooks/useInitialValues";
+import {Formik, Form} from "formik";
+import validationSchema from "./validationSchema";
+import {Button} from "flowbite-react";
+import ExperienceForm from "./experience-form/experience-form";
 
 export function App() {
-  return (
-      <>
-        <PersonalForm/>
-        <ExperiencesForm/>
-        {/*<ExperienceForm/>*/}
-      </>
-  );
+    const initialValues = useInitialValues();
+    const handleSubmit = (e: any) => console.log(e);
+    return (
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+        >
+            {({ values: {personalDetails, experiences}, handleSubmit, isSubmitting, handleChange}) => (
+                <Form>
+                    <PersonalForm values={personalDetails} handleChange={handleChange}/>
+                    <ExperienceForm values={experiences} handleChange={handleChange}/>
+                    <Button type="submit">Submit</Button>
+                </Form>
+            )}
+        </Formik>
+    )
 }
 
 export default App;
