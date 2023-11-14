711fe18cb5d338531978767ef1e509bc851da9b9
EcclesN <Niall.Eccles@hmhco.com>
2023-11-14 23:01:02 +0000

    Create validation schema using Yup

---
+++ src/app/validationSchema.ts
@@ -0,0 +1,26 @@
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+    personalDetails: Yup.object().shape({
+        firstName: Yup.string(),
+        lastName: Yup.string(),
+        jobTitle: Yup.string(),
+        email: Yup.string(),
+        phone: Yup.string(),
+        country: Yup.string(),
+        city: Yup.string(),
+        summary: Yup.string(),
+    }),
+    experiences: Yup.array().of(
+        Yup.object().shape({
+            school: Yup.string(),
+            degree: Yup.string(),
+            currentlyStudying: Yup.boolean(),
+            startDate: Yup.string(),
+            endDate: Yup.string(),
+            description: Yup.string(),
+        })
+    )
+});
+
+export default validationSchema;
