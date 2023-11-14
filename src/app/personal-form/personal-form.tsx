41559e95db4c2e3c61d1aaf6c27b9b86ddb20060
EcclesN <Niall.Eccles@hmhco.com>
2023-11-14 23:01:34 +0000

    Rewrite personal form to be leaner

--- src/app/personal-form/personal-form.tsx
+++ src/app/personal-form/personal-form.tsx
@@ -1,39 +1,27 @@
 import styles from './personal-form.module.css';
 import React from "react";
-import {Form, Formik, FieldArray, Field} from "formik";
-import { Button, Label, TextInput, Checkbox } from 'flowbite-react';
-import {ExperienceFields, PersonalFields} from "../types";
-import useInitialValues from "../hooks/useInitialValues";
+import { Label, TextInput } from 'flowbite-react';
+import {PersonalFields} from "../types";
 
 /* eslint-disable-next-line */
-export interface PersonalFormProps {}
+export interface PersonalFormProps {
+    values: PersonalFields,
+    handleChange: {(e: React.ChangeEvent<any>): void, <T=string | React.ChangeEvent<any>>(field: T): T extends React.ChangeEvent<any> ? void : ((e: (string | React.ChangeEvent<any>)) => void)}
+}
 
 export function PersonalForm(props: PersonalFormProps) {
-    const initialValues: PersonalFields = useInitialValues();
 
   return (
     <div className={styles['container']}>
         <h2>Personal Details</h2>
-        <Formik
-            initialValues={initialValues}
-            onSubmit={(values) => {
-                console.log(values);
-            }}
-        >
-            {({ values: {personalDetails} , handleChange}) => (
-                <Form>
-                    <Label htmlFor={`personalDetails.firstName`} value="First Name"/>
-                    <TextInput
-                        name={`personalDetails.firstName`}
-                        id={`personalDetails.firstName`}
-                        value={personalDetails.firstName}
-                        placeholder="First Name"
-                        onChange={handleChange}
-                    />
-                    <Button type="submit">Submit</Button>
-                </Form>
-            )}
-        </Formik>
+        <Label htmlFor={`personalDetails.firstName`} value="First Name"/>
+        <TextInput
+            name={`personalDetails.firstName`}
+            id={`personalDetails.firstName`}
+            value={props.values.firstName}
+            placeholder="First Name"
+            onChange={props.handleChange}
+        />
     </div>
   );
 }
