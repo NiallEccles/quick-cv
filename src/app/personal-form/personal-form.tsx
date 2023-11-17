0bf6e2c7fa4537ab9774ff0d5dcbc646d24d0b6e
EcclesN <Niall.Eccles@hmhco.com>
2023-11-17 22:34:37 +0000

    Add more fields to personal form

--- src/app/personal-form/personal-form.tsx
+++ src/app/personal-form/personal-form.tsx
@@ -14,30 +14,69 @@ export function PersonalForm(props: PersonalFormProps) {
   return (
     <div className={styles['container']}>
         <Title order={2} size="h3" mb='sm'>Personal Details</Title>
-        <Divider my="sm" />
+        <Divider my="sm" variant="dashed" />
         <Group grow>
             <Stack gap="xs">
-                <label htmlFor="personalDetails.firstName">
-                    <Title order={3} size="h4">First Name</Title>
+                <label htmlFor="personalDetails.name">
+                    <Title order={3} size="h4">Name</Title>
                 </label>
                 <Input
-                    name={`personalDetails.firstName`}
-                    id={`personalDetails.firstName`}
-                    value={props.values.firstName}
-                    placeholder="First Name"
+                    name={`personalDetails.name`}
+                    id={`personalDetails.name`}
+                    value={props.values.name}
+                    placeholder="Name"
                     onChange={props.handleChange}
                 />
             </Stack>
-
+        </Group>
+        <Group grow mt="sm">
             <Stack gap="xs">
-                <label htmlFor="personalDetails.lastName">
-                    <Title order={3} size="h4">Last Name</Title>
+                <label htmlFor="personalDetails.email">
+                    <Title order={3} size="h4">Email</Title>
                 </label>
                 <Input
-                    name={`personalDetails.lastName`}
-                    id={`personalDetails.lastName`}
-                    value={props.values.lastName}
-                    placeholder="Last Name"
+                    name={`personalDetails.email`}
+                    id={`personalDetails.email`}
+                    value={props.values.email}
+                    placeholder="Email"
+                    onChange={props.handleChange}
+                />
+            </Stack>
+            <Stack gap="xs">
+                <label htmlFor="personalDetails.phone">
+                    <Title order={3} size="h4">Phone</Title>
+                </label>
+                <Input
+                    name={`personalDetails.phone`}
+                    id={`personalDetails.phone`}
+                    value={props.values.phone}
+                    placeholder="Phone"
+                    onChange={props.handleChange}
+                />
+            </Stack>
+        </Group>
+        <Group grow mt="sm">
+            <Stack gap="xs">
+                <label htmlFor="personalDetails.country">
+                    <Title order={3} size="h4">Country</Title>
+                </label>
+                <Input
+                    name={`personalDetails.country`}
+                    id={`personalDetails.country`}
+                    value={props.values.country}
+                    placeholder="Country"
+                    onChange={props.handleChange}
+                />
+            </Stack>
+            <Stack gap="xs">
+                <label htmlFor="personalDetails.city">
+                    <Title order={3} size="h4">City</Title>
+                </label>
+                <Input
+                    name={`personalDetails.city`}
+                    id={`personalDetails.city`}
+                    value={props.values.city}
+                    placeholder="City"
                     onChange={props.handleChange}
                 />
             </Stack>
